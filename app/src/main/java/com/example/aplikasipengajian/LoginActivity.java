16dfbc2c61b0db1eb0068debd19be982d37189a4
taruna <taruna.rachmad@gmail.com>
2019-11-12 22:08:04 +0700

    menambahkan layout login

--- app/src/main/java/com/example/aplikasipengajian/LoginActivity.java
+++ app/src/main/java/com/example/aplikasipengajian/LoginActivity.java
@@ -2,8 +2,12 @@ package com.example.aplikasipengajian;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import retrofit2.Call;
 
 public class LoginActivity extends AppCompatActivity {
     Button buttonLogin;
@@ -13,6 +17,19 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        buttonLogin = findViewById(R.id.btn_sign_in);
 
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    public void login(String username,String password){
+        PengajianApi pengajianApi = ServiceGenerator.getPengajianApi();
+        Call<JadwalPengajianResponse> jadwalPengajianResponseCall = pengajianApi.userLogin(username,password);
     }
 }
